b73605fa8c064fd0d8901fee479fbe58d7f2e624
msaaddev <mrsaadirfan@gmail.com>
2021-10-09 00:41:19 +0500

    📦 NEW: ui implemented

--- weather-app/pages/index.js
+++ weather-app/pages/index.js
@@ -1,12 +1,8 @@
-import Head from 'next/head';
+import { useState } from 'react';
 
 export default function Home() {
-	return (
-		<div className="flex flex-col items-center justify-center min-h-screen py-2">
-			<Head>
-				<title>Create Next App</title>
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
+	const [city, setCity] = useState('');
+	const [weather, setWeather] = useState(null);
 
 			<main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
 				<h1 className="text-6xl font-bold">
@@ -16,77 +12,31 @@ export default function Home() {
 					</a>
 				</h1>
 
-				<p className="mt-3 text-2xl">
-					Get started by editing{' '}
-					<code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-						pages/index.js
-					</code>
-				</p>
-
-				<div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-					<a
-						href="https://nextjs.org/docs"
-						className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-					>
-						<h3 className="text-2xl font-bold">
-							Documentation &rarr;
-						</h3>
-						<p className="mt-4 text-xl">
-							Find in-depth information about Next.js features and
-							API.
-						</p>
-					</a>
-
-					<a
-						href="https://nextjs.org/learn"
-						className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-					>
-						<h3 className="text-2xl font-bold">Learn &rarr;</h3>
-						<p className="mt-4 text-xl">
-							Learn about Next.js in an interactive course with
-							quizzes!
-						</p>
-					</a>
-
-					<a
-						href="https://github.com/vercel/next.js/tree/master/examples"
-						className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-					>
-						<h3 className="text-2xl font-bold">Examples &rarr;</h3>
-						<p className="mt-4 text-xl">
-							Discover and deploy boilerplate example Next.js
-							projects.
-						</p>
-					</a>
-
-					<a
-						href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-						className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-					>
-						<h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-						<p className="mt-4 text-xl">
-							Instantly deploy your Next.js site to a public URL
-							with Vercel.
-						</p>
-					</a>
+	return (
+		<div className="flex justify-center items-center h-screen flex-col">
+			<div>
+				<h2 className="font-raleway text-5xl font-extrabold mb-10">
+					Weather App
+				</h2>
+			</div>
+			<div className="flex">
+				<input
+					type="text"
+					placeholder="City..."
+					className="outline-indigo mr-6 rounded-sm pl-4 w-64 font-raleway"
+					onChange={e => setCity(e.target.value)}
+				/>
+				<button className="outline-none border-none font-bold font-raleway px-12 py-2 rounded-sm bg-indigo-300 text-gray-700 transition duration-300 hover:bg-indigo-600 hover:text-white">
+					Search
+				</button>
+			</div>
+			{weather && (
+				<div className="mt-10">
+					<p className="font-raleway text-2xl font-semibold">
+						Temperature: {weather}
+					</p>
 				</div>
-			</main>
-
-			<footer className="flex items-center justify-center w-full h-24 border-t">
-				<a
-					className="flex items-center justify-center"
-					href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Powered by{' '}
-					<img
-						src="/vercel.svg"
-						alt="Vercel Logo"
-						className="h-4 ml-2"
-					/>
-				</a>
-			</footer>
+			)}
 		</div>
 	);
 }
--- weather-app/tailwind.config.js
+++ weather-app/tailwind.config.js
@@ -1,12 +1,20 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+	mode: 'jit',
+	purge: [
+		'./pages/**/*.{js,ts,jsx,tsx}',
+		'./components/**/*.{js,ts,jsx,tsx}'
+	],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		fontFamily: {
+			raleway: ['Raleway', 'sans-serif']
+		},
+		outline: {
+			indigo: '2px solid #A8B4F6'
+		}
+	},
+	variants: {
+		extend: {}
+	},
+	plugins: []
+};
