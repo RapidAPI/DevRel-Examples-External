11d576a344667b76b3b356daefa9e66f50b8531a
msaaddev <mrsaadirfan@gmail.com>
2021-10-09 01:12:27 +0500

    📦 NEW: fetching weather results

--- weather-app/pages/index.js
+++ weather-app/pages/index.js
@@ -1,16 +1,44 @@
 import { useState } from 'react';
+import axios from 'axios';
 
 export default function Home() {
 	const [city, setCity] = useState('');
-	const [weather, setWeather] = useState(null);
+	const [temp, setTemp] = useState(null);
+	const [minTemp, setMinTemp] = useState('');
+	const [maxTemp, setMaxTemp] = useState('');
 
-			<main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-				<h1 className="text-6xl font-bold">
-					Welcome to{' '}
-					<a className="text-blue-600" href="https://nextjs.org">
-						Next.js!
-					</a>
-				</h1>
+	/**
+	 *
+	 * fetch weather information of the given city
+	 */
+	const getWeather = () => {
+		const options = {
+			method: 'GET',
+			url: 'https://community-open-weather-map.p.rapidapi.com/weather',
+			params: { q: `${city}`, units: 'metric' },
+			headers: {
+				'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com',
+				'x-rapidapi-key': process.env.NEXT_PUBLIC_OPEN_WEATHER_API_KEY
+			}
+		};
+
+		axios
+			.request(options)
+			.then(function (response) {
+				console.log(response.data);
+				const { data } = response;
+				const newTemp = Math.ceil(data.main.temp);
+				const newMinTemp = Math.ceil(data.main.temp_min);
+				const newMaxTemp = Math.ceil(data.main.temp_max);
+
+				setTemp(newTemp);
+				setMinTemp(newMinTemp);
+				setMaxTemp(newMaxTemp);
+			})
+			.catch(function (error) {
+				console.error(error);
+			});
+	};
 
 	return (
 		<div className="flex justify-center items-center h-screen flex-col">
@@ -26,15 +54,27 @@ export default function Home() {
 					className="outline-indigo mr-6 rounded-sm pl-4 w-64 font-raleway"
 					onChange={e => setCity(e.target.value)}
 				/>
-				<button className="outline-none border-none font-bold font-raleway px-12 py-2 rounded-sm bg-indigo-300 text-gray-700 transition duration-300 hover:bg-indigo-600 hover:text-white">
+				<button
+					onClick={getWeather}
+					className="outline-none border-none font-bold font-raleway px-12 py-2 rounded-sm bg-indigo-300 text-gray-700 transition duration-300 hover:bg-indigo-600 hover:text-white"
+				>
 					Search
 				</button>
 			</div>
-			{weather && (
-				<div className="mt-10">
-					<p className="font-raleway text-2xl font-semibold">
-						Temperature: {weather}
-					</p>
+			{temp && (
+				<div className="mt-10 flex flex-col justify-start bg-indigo-200 px-12 py-4 rounded font-raleway text-xl font-semibold text-gray-700">
+					<div className="flex mb-4">
+						<p className="w-64">Temperature:</p>
+						<p>{temp} ° C</p>
+					</div>
+					<div className="flex mb-4">
+						<p className="w-64">Temperature Min:</p>
+						<p>{minTemp}° C</p>
+					</div>
+					<div className="flex">
+						<p className="w-64">Temperature Max:</p>
+						<p>{maxTemp}° C</p>
+					</div>
 				</div>
 			)}
 		</div>
