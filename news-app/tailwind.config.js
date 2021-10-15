c5d8d97ed4085861b43bc8f5683191143a60a71f
msaaddev <mrsaadirfan@gmail.com>
2021-10-15 22:48:21 +0500

    📦 NEW: Screen and color configs

--- news-app/tailwind.config.js
+++ news-app/tailwind.config.js
@@ -1,12 +1,27 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    screens: {
+      xl: { max: "1279px" },
+      lg: { max: "1023px" },
+      md: { max: "767px" },
+      sm: { max: "639px" },
+    },
+    fontFamily: {
+      raleway: ["Raleway", "sans-serif"],
+    },
+    colors: {
+      bc: "#000000",
+      primary: "#FFFDFB",
+      secondary: "#D6E3E4",
+      danger: " #31CB53",
+      btnColor: " #F6FF81",
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
