83d25fd9c9246def67f0003954f1631c583fe0e8
msaaddev <mrsaadirfan@gmail.com>
2021-11-04 04:45:09 +0500

    📦 NEW: Added new configs

--- paraphrase-app/tailwind.config.js
+++ paraphrase-app/tailwind.config.js
@@ -1,12 +1,24 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    screens: {
+      md: { max: "767px" },
+    },
+    fontFamily: {
+      raleway: ["Raleway", "sans-serif"],
+    },
+    colors: {
+      bc: "#FEFEFE",
+      primary: "#1C1C1C",
+      secondary: "#DD8B6B",
+      danger: "#81978B",
+      lightBlue: "#A0BEE6",
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
