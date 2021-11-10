f0d038a25e2f52699435fa57a71e2dd9cc698a97
msaaddev <mrsaadirfan@gmail.com>
2021-11-11 01:44:59 +0500

    📦 NEW: Added config

--- text-extraction-app/tailwind.config.js
+++ text-extraction-app/tailwind.config.js
@@ -1,12 +1,25 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    screens: {
+      md: { max: "767px" },
+    },
+    fontFamily: {
+      raleway: ["Raleway", "sans-serif"],
+    },
+    colors: {
+      bc: "#08243B",
+      primary: "#000000",
+      secondary: "#D4D4D6",
+      active: "#C79360",
+      lightGrey: "#5E6F7C",
+      danger: " #756151",
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
