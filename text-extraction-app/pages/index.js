6d8d26186e28b50caea4fd9b972d350a3f4422a7
msaaddev <mrsaadirfan@gmail.com>
2021-11-11 02:04:41 +0500

    📦 NEW: Integrated API

--- text-extraction-app/pages/index.js
+++ text-extraction-app/pages/index.js
@@ -1,7 +1,28 @@
 import { useState } from "react";
+import axios from "axios";
 
 export default function Home() {
+  const [url, setUrl] = useState("");
   const [extractedText, setExtractedText] = useState(null);
+
+  /**
+   *
+   *
+   * Fetch extracted content from the url
+   */
+  const fetchExtractedContent = async () => {
+    try {
+      const res = await axios.get(`/api/extract`, {
+        params: { url },
+      });
+      const { data } = res;
+      const { text } = data;
+
+      setExtractedText(text);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <div className="flex flex-col items-center relative min-h-screen">
@@ -17,8 +38,12 @@ export default function Home() {
             type="text"
             className="border border-primary outline-none w-2/5 px-4 py-2 rounded-sm bg-lightGrey text-secondary font-bold font-raleway md:w-full"
             placeholder="Paste the article link..."
+            onChange={(e) => setUrl(e.target.value)}
           />
-          <button className="outline-none border border-active font-bold font-raleway ml-4 px-12 py-2 rounded-sm bg-active text-primary transition duration-300 hover:bg-bc hover:text-secondary hover:border-secondary md:h-16 md:my-12">
+          <button
+            className="outline-none border border-active font-bold font-raleway ml-4 px-12 py-2 rounded-sm bg-active text-primary transition duration-300 hover:bg-bc hover:text-secondary hover:border-secondary md:h-16 md:my-12"
+            onClick={fetchExtractedContent}
+          >
             Extract Text
           </button>
         </div>
@@ -27,6 +52,7 @@ export default function Home() {
           <textarea
             type="text"
             className="bg-lightGrey text-secondary border border-primary outline-none w-3/5 h-96 mt-12 px-4 py-2 rounded-sm font-raleway md:w-full"
+            value={extractedText}
           />
         )}
       </div>
