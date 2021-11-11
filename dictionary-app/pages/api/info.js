fb1a34c5dfaf7bbdb3437fc0d8716b9cdda16d40
msaaddev <mrsaadirfan@gmail.com>
2021-11-12 00:11:36 +0500

    📦 NEW: API route

---
+++ dictionary-app/pages/api/info.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+export default async function handler(req, res) {
+  if (req.method === "GET") {
+    const options = {
+      method: "GET",
+      url: "https://dictionary-by-api-ninjas.p.rapidapi.com/v1/dictionary",
+      params: { word: req.query.word },
+      headers: {
+        "x-rapidapi-host": "dictionary-by-api-ninjas.p.rapidapi.com",
+        "x-rapidapi-key": process.env.NEXT_PUBLIC_RAPIDAPI_KEY,
+      },
+    };
+
+    axios
+      .request(options)
+      .then(function (response) {
+        res.status(200).json(response.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  } else {
+    res.status(400);
+  }
+}
