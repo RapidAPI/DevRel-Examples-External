c1096e0cf1d5251495ecfb4d2a6eeccbe31a355f
msaaddev <mrsaadirfan@gmail.com>
2021-11-12 00:14:56 +0500

    📦 NEW: Integrated API

--- dictionary-app/pages/index.js
+++ dictionary-app/pages/index.js
@@ -1,20 +1,29 @@
-import Head from 'next/head'
+import { useState } from "react";
+import axios from "axios";
 
 export default function Home() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+  const [word, setWord] = useState("");
+  const [wordInfo, setWordInfo] = useState(null);
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+  /**
+   *
+   *
+   * Fetch word information
+   */
+  const fetchInfo = async () => {
+    try {
+      const res = await axios.get(`/api/info`, {
+        params: { word },
+      });
+      const { data } = res;
+      const { definition } = data;
+
+      const newDefinition = definition.split(/1. |2. | 3. /);
+      setWordInfo(newDefinition);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <div className="flex flex-col items-center relative min-h-screen">
@@ -45,7 +54,7 @@ export default function Home() {
               <thead className="font-raleway uppercase tracking-wide">
                 <tr>
                   <th className="border text-left px-4 py-4">
-                    <span className="text-secondary">Type</span>
+                    <span className="text-secondary">{word}</span>
                   </th>
                   <th className="border text-left px-4 py-4">
                     <span className="text-secondary">Definition</span>
@@ -54,11 +63,12 @@ export default function Home() {
               </thead>
               <tbody>
                 <tr>
-                  <td className="border px-4 py-4">Noun</td>
-                  <td className="border px-4 py-4">
-                    A written or printed work consisting of pages glued or sewn
-                    together along one side and bound in covers.
-                  </td>
+                  <td className="border px-4 py-4">1.</td>
+                  <td className="border px-4 py-4">{wordInfo[1]}</td>
+                </tr>
+                <tr>
+                  <td className="border px-4 py-4">2.</td>
+                  <td className="border px-4 py-4">{wordInfo[2]}</td>
                 </tr>
               </tbody>
             </table>
