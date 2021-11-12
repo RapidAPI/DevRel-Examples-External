5be830cacda4ec68ada8a9d73c6f75c0ac13d1e5
msaaddev <mrsaadirfan@gmail.com>
2021-11-13 00:09:45 +0500

    📦 NEW: API route

---
+++ superhero-app/pages/api/superhero.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+export default async function handler(req, res) {
+  if (req.method === "GET") {
+    const options = {
+      method: "GET",
+      url: "https://superhero-search.p.rapidapi.com/api/",
+      params: { hero: req.query.name },
+      headers: {
+        "x-rapidapi-host": "superhero-search.p.rapidapi.com",
+        "x-rapidapi-key": process.env.NEXT_PUBLIC_RAPIDAPI_KEY,
+      },
+    };
+
+    axios
+      .request(options)
+      .then(function (response) {
+        res.status(200).json(response.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  } else {
+    res.status(400);
+  }
+}
