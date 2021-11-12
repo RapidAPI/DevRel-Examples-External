c0601b6ab309286f8b6542cc59aae185a44ca5eb
msaaddev <mrsaadirfan@gmail.com>
2021-11-12 23:20:34 +0500

    📦 NEW: Created UI

--- superhero-app/pages/index.js
+++ superhero-app/pages/index.js
@@ -1,4 +1,4 @@
-import Head from 'next/head'
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -8,75 +8,83 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+  return (
+    <div className="flex flex-col items-center relative min-h-screen">
+      <h2 className="font-raleway font-bold text-6xl text-primary pt-20 pb-6 md:text-3xl">
+        <span className="text-active">Superhero</span> App
+      </h2>
+      <h3 className="text-secondary text-2xl font-raleway font-bold uppercase tracking-wide mb-12 md:text-base md:px-4 md:text-center">
+        Search for your favorite superhero
+      </h3>
+      <div className="flex flex-col justify-between items-center w-full md:items-center">
+        <form className="flex w-full justify-center md:flex-col md:w-5/6 ">
+          <input
+            autoFocus={true}
+            type="text"
+            className="border-none outline-none w-2/5 bg-primary px-4 py-2 rounded-sm font-raleway md:w-full"
+            placeholder="Enter the name of superhero..."
+          />
+          <button className="outline-none border border-active font-bold font-raleway ml-4 px-12 py-2 rounded-sm bg-active text-primary transition duration-300 hover:bg-background hover:text-black md:ml-0 md:mt-4">
+            Search
+          </button>
+        </form>
+        <div className="flex mt-12 w-3/6 h-4/5 border border-primary md:flex-col md:w-4/6 md:h-full md:mb-12">
+          <Image
+            src="https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/images/lg/346-iron-man.jpg"
+            width={220}
+            height={300}
+            alt="{movieInfo.title}"
+          />
+          <div className="flex flex-col justify-center ml-8 md:mt-6 w-3/6">
+            <div className="flex justify-between w-full">
+              <p className="text-active uppercase text-raleway font-bold tracking-wider md:text-base">
+                Real Name
+              </p>
+              <p className="text-secondary">Tony Stark </p>
+            </div>
+            <div className="flex justify-between mt-4 w-full">
+              <p className="text-active uppercase text-raleway font-bold tracking-wider md:text-base">
+                Superhero Name
+              </p>
+              <p className="text-secondary">Iron Man </p>
+            </div>
+            <div className="flex justify-between mt-4 w-full">
+              <p className="text-active uppercase text-raleway font-bold tracking-wider md:text-base">
+                Intelligence
+              </p>
+              <p className="text-secondary">100 </p>
+            </div>
+            <div className="flex justify-between mt-4 w-full">
+              <p className="text-active uppercase text-raleway font-bold tracking-wider md:text-base">
+                Strength
+              </p>
+              <p className="text-secondary">84 </p>
+            </div>
+            <div className="flex justify-between mt-4 w-full">
+              <p className="text-active uppercase text-raleway font-bold tracking-wider md:text-base">
+                Speed
+              </p>
+              <p className="text-secondary">15 </p>
+            </div>
+            <div className="flex justify-between mt-4 w-full">
+              <p className="text-active uppercase text-raleway font-bold tracking-wider md:text-base">
+                Durability
+              </p>
+              <p className="text-secondary">314 </p>
+            </div>
+          </div>
         </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
+      </div>
+      <div className="absolute bottom-0 flex justify-center items-end h-36 md:h-44">
+        <p className="text-primary pb-12 md:w-60 md:text-center">
+          Made by RapidAPI DevRel Team –{" "}
+          <a href="https://github.com/RapidAPI/DevRel-Examples-External">
+            <span className="transition duration-300 hover:text-secondary">
+              See Examples Like this
+            </span>
+          </a>
+        </p>
+      </div>
     </div>
-  )
+  );
 }
