0c4eb531c0036a2167112700598c097df69e0624
msaaddev <mrsaadirfan@gmail.com>
2021-11-12 22:43:37 +0500

    📦 NEW: Updated config

--- superhero-app/tailwind.config.js
+++ superhero-app/tailwind.config.js
@@ -1,12 +1,25 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    screens: {
+      md: { max: "767px" },
+    },
+    fontFamily: {
+      raleway: ["Raleway", "sans-serif"],
+    },
+    colors: {
+      background: "#000000",
+      primary: "#FFFFFF",
+      secondary: "#9E9E9E",
+      active: "#1BB954",
+      light: "#121212",
+    },
   },
+
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
