0334a9f6659a70723da8ab07225bfae869cccc76
msaaddev <mrsaadirfan@gmail.com>
2021-11-19 00:32:32 +0500

    📦 NEW: Added UI and Integrated API

--- time-app/pages/index.js
+++ time-app/pages/index.js
@@ -1,82 +1,87 @@
-import Head from 'next/head'
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-export default function Home() {
+export default function Home({ value }) {
+  const [utcTime, setUtcTime] = useState(null);
+  const [utcDate, setUtcDate] = useState(null);
+
+  useEffect(() => {
+    const date = value.utc_datetime.slice(0, 10);
+    const time = value.utc_datetime.slice(11, 16);
+
+    setUtcDate(date);
+    setUtcTime(time);
+  }, [utcTime, utcDate]);
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
+    <div className="flex flex-col items-center relative min-h-screen">
+      <h2 className="font-raleway font-bold text-6xl text-primary pt-20 pb-6 md:text-3xl">
+        <span className="text-danger">Time</span> App
+      </h2>
+      <h3 className="text-danger text-2xl font-raleway font-bold uppercase tracking-wide mb-4 md:text-base md:px-4 md:text-center">
+        Get Time-related information according to your area
+      </h3>
+      <div className="flex flex-col text-primary text-raleway mt-12 w-3/6 h-4/5  md:flex-col md:w-4/6 md:h-full md:mb-12">
+        <table className="bg-white w-full text-primary mb-8 md:text-sm md:mx-2">
+          <thead className="font-raleway uppercase tracking-wide">
+            <tr>
+              <th className="border text-left px-4 py-4">
+                <span className="text-danger">Information</span>
+              </th>
+              <th className="border text-left px-4 py-4">
+                <span className="text-danger">Value</span>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="border px-4 py-4">Timezone</td>
+              <td className="border px-4 py-4">{value.timezone}</td>
+            </tr>
+            <tr>
+              <td className="border px-4 py-4">UTC Time</td>
+              <td className="border px-4 py-4">{utcTime}</td>
+            </tr>
+            <tr>
+              <td className="border px-4 py-4">UTC Date</td>
+              <td className="border px-4 py-4">{utcDate}</td>
+            </tr>
+            <tr>
+              <td className="border px-4 py-4">UTC Offset</td>
+              <td className="border px-4 py-4">{value.utc_offset}</td>
+            </tr>
+            <tr>
+              <td className="border px-4 py-4">Client IP</td>
+              <td className="border px-4 py-4">{value.client_ip}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div className="absolute bottom-0 flex justify-center items-end h-52 md:h-44">
+        <p className="text-primary pb-12 md:w-60 md:text-center">
+          Made by RapidAPI DevRel Team –{" "}
+          <a href="https://github.com/RapidAPI/DevRel-Examples-External">
+            See Examples Like this
           </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
         </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
+      </div>
     </div>
-  )
+  );
+}
+
+export async function getServerSideProps() {
+  const res = await axios.get("http://localhost:3000/api/time");
+  const { data: value } = res;
+
+  if (!value) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      value,
+    },
+  };
 }
