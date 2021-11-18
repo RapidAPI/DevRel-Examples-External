1a91cf7eebd5a5523830a44e86a4ef0af0398b9b
msaaddev <mrsaadirfan@gmail.com>
2021-11-18 23:49:19 +0500

    📦 NEW: First commit

---
+++ time-app/tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    screens: {
+      md: { max: "767px" },
+    },
+    fontFamily: {
+      raleway: ["Raleway", "sans-serif"],
+    },
+    colors: {
+      bc: "#353849",
+      primary: "#FFFFFF",
+      secondary: "#232734",
+      danger: "#03CC90",
+      lightGrey: "#3F4354",
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
