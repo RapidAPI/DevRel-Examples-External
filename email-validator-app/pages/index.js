4c48a594fe87c8f0d2b8619e5f5a012e020240e3
msaaddev <mrsaadirfan@gmail.com>
2021-11-25 22:33:26 +0500

    📦 NEW: Created UI

--- email-validator-app/pages/index.js
+++ email-validator-app/pages/index.js
@@ -1,82 +1,67 @@
-import Head from 'next/head'
+import { useState } from "react";
 
 export default function Home() {
+  const [email, setEmail] = useState("");
+  const [res, setRes] = useState(true);
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
+    <div className="flex flex-col items-center relative min-h-screen">
+      <h2 className="font-raleway font-bold text-6xl text-primary pt-20 pb-6 md:text-3xl">
+        Email <span className="text-secondary">Validator</span> App
+      </h2>
+      <h3 className="text-lightGrey text-2xl font-raleway font-bold uppercase tracking-wide mb-12 md:text-base md:px-4 md:text-center">
+        Check if an email address exists or not
+      </h3>
+      <div className="flex flex-col justify-between items-center w-full md:items-center">
+        <form className="flex w-full justify-center md:flex-col md:w-5/6 ">
+          <input
+            autoFocus={true}
+            type="email"
+            className="border-none outline-none w-2/5 bg-primary px-4 py-2 rounded-sm font-raleway md:w-full"
+            placeholder="Enter the email address..."
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <button className="outline-none border border-danger font-bold font-raleway ml-4 px-12 py-2 rounded-sm bg-danger text-primary transition duration-300 hover:bg-bc hover:text-black md:ml-0 md:mt-4">
+            Validate
+          </button>
+        </form>
+        {res && (
+          <div className="flex flex-col text-primary text-raleway mt-12 w-3/6 h-4/5  md:flex-col md:w-4/6 md:h-full md:mb-12">
+            <table className="bg-white w-full text-primary mb-8 md:text-sm md:mx-2">
+              <thead className="font-raleway uppercase tracking-wide">
+                <tr>
+                  <th className="border text-left px-4 py-4">
+                    <span className="text-secondary">Information</span>
+                  </th>
+                  <th className="border text-left px-4 py-4">
+                    <span className="text-secondary">Result</span>
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td className="border px-4 py-4">Hello</td>
+                  <td className="border px-4 py-4">Hello</td>
+                </tr>
+                <tr>
+                  <td className="border px-4 py-4">Hello</td>
+                  <td className="border px-4 py-4">Hello</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        )}
+      </div>
+      <div className="absolute bottom-0 flex justify-center items-end h-36 md:h-44">
+        <p className="text-primary pb-12 md:w-60 md:text-center">
+          Made by RapidAPI DevRel Team –{" "}
+          <a href="https://github.com/RapidAPI/DevRel-Examples-External">
+            <span className="transition duration-300 hover:text-secondary">
+              See Examples Like this
+            </span>
           </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
         </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
+      </div>
     </div>
-  )
+  );
 }
