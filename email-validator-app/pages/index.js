4681fcb9dd5a918a9657e86c6faf41b04563fb10
msaaddev <mrsaadirfan@gmail.com>
2021-11-25 23:25:56 +0500

    📦 NEW: Integrated API

--- email-validator-app/pages/index.js
+++ email-validator-app/pages/index.js
@@ -1,8 +1,27 @@
 import { useState } from "react";
+import axios from "axios";
 
 export default function Home() {
   const [email, setEmail] = useState("");
-  const [res, setRes] = useState(true);
+  const [res, setRes] = useState(false);
+
+  /**
+   *
+   *
+   * Checks validatity of en email
+   */
+  const checkValidity = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.get(`/api/validate`, {
+        params: { email },
+      });
+      const { data } = res;
+      setRes(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <div className="flex flex-col items-center relative min-h-screen">
@@ -13,7 +32,10 @@ export default function Home() {
         Check if an email address exists or not
       </h3>
       <div className="flex flex-col justify-between items-center w-full md:items-center">
-        <form className="flex w-full justify-center md:flex-col md:w-5/6 ">
+        <form
+          className="flex w-full justify-center md:flex-col md:w-5/6"
+          onSubmit={(e) => checkValidity(e)}
+        >
           <input
             autoFocus={true}
             type="email"
@@ -21,7 +43,10 @@ export default function Home() {
             placeholder="Enter the email address..."
             onChange={(e) => setEmail(e.target.value)}
           />
-          <button className="outline-none border border-danger font-bold font-raleway ml-4 px-12 py-2 rounded-sm bg-danger text-primary transition duration-300 hover:bg-bc hover:text-black md:ml-0 md:mt-4">
+          <button
+            className="outline-none border border-danger font-bold font-raleway ml-4 px-12 py-2 rounded-sm bg-danger text-primary transition duration-300 hover:bg-bc hover:text-black md:ml-0 md:mt-4"
+            onClick={checkValidity}
+          >
             Validate
           </button>
         </form>
@@ -40,19 +65,35 @@ export default function Home() {
               </thead>
               <tbody>
                 <tr>
-                  <td className="border px-4 py-4">Hello</td>
-                  <td className="border px-4 py-4">Hello</td>
+                  <td className="border px-4 py-4">Valid</td>
+                  <td className="border px-4 py-4 capitalize">
+                    {res.valid.toString()}
+                  </td>
                 </tr>
                 <tr>
-                  <td className="border px-4 py-4">Hello</td>
-                  <td className="border px-4 py-4">Hello</td>
+                  <td className="border px-4 py-4">Disposible</td>
+                  <td className="border px-4 py-4 capitalize">
+                    {res.disposable.toString()}
+                  </td>
+                </tr>
+                <tr>
+                  <td className="border px-4 py-4">Domain</td>
+                  <td className="border px-4 py-4">{res.domain}</td>
+                </tr>
+                <tr>
+                  <td className="border px-4 py-4">Text</td>
+                  <td className="border px-4 py-4">{res.text}</td>
+                </tr>
+                <tr>
+                  <td className="border px-4 py-4">Reason</td>
+                  <td className="border px-4 py-4">{res.reason}</td>
                 </tr>
               </tbody>
             </table>
           </div>
         )}
       </div>
-      <div className="absolute bottom-0 flex justify-center items-end h-36 md:h-44">
+      <div className="absolute bottom-0 mt-8 flex justify-center items-end h-36 md:h-44">
         <p className="text-primary pb-12 md:w-60 md:text-center">
           Made by RapidAPI DevRel Team –{" "}
           <a href="https://github.com/RapidAPI/DevRel-Examples-External">
