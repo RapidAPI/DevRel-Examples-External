acbd41585ddf4aca6156605d7525954ad92adfd5
msaaddev <mrsaadirfan@gmail.com>
2021-11-27 01:28:03 +0500

    📦 NEW: Integrated API

--- language-detection-app/pages/index.js
+++ language-detection-app/pages/index.js
@@ -1,6 +1,33 @@
-import Head from "next/head";
+import { useState } from "react";
+import axios from "axios";
 
-export default function Home() {
+export default function Home({ value: lang }) {
+  const [text, setText] = useState("");
+  const [detectedLang, setDetectedLang] = useState("");
+
+  /**
+   *
+   *
+   * Detect text language
+   */
+  const fetchTextInfo = async () => {
+    try {
+      setDetectedLang(`Detecting language...`);
+      const res = await axios.post(`/api/detect`, { text });
+      const { data } = res;
+      setDetectedLang(
+        `The text is written in ${
+          lang.dictionary[data[0].language].name
+        } language. And the language's native name is ${
+          lang.dictionary[data[0].language].nativeName
+        }.`
+      );
+    } catch (err) {
+      setDetectedLang(`Language couldn't be detected.`);
+      console.log(err);
+    }
+  };
+
   return (
     <div className="flex flex-col items-center relative min-h-screen">
       <h2 className="font-raleway font-bold text-6xl text-primary pt-20 pb-6 md:text-3xl">
@@ -14,16 +41,21 @@ export default function Home() {
           type="text"
           className="border border-primary outline-none w-2/5 px-4 py-2 rounded-sm font-raleway bg-bc text-primary md:w-full"
           placeholder="Write/paste any content..."
+          onChange={(e) => setText(e.target.value)}
         />
         <div className="flex items-center">
-          <button className="h-1/6 outline-none border border-danger font-bold font-raleway mx-12 px-12 rounded-sm bg-danger text-bc transition duration-300 hover:bg-bc hover:text-primary md:h-16 md:my-12">
+          <button
+            className="h-1/6 outline-none border border-danger font-bold font-raleway mx-12 px-12 rounded-sm bg-danger text-bc transition duration-300 hover:bg-bc hover:text-primary md:h-16 md:my-12"
+            onClick={fetchTextInfo}
+          >
             Detect
           </button>
         </div>
         <textarea
           type="text"
           className="border border-primary outline-none w-2/5  px-4 py-2 rounded-sm font-raleway bg-bc text-primary md:w-full"
-          placeholder="Paraphrased content"
+          placeholder="Text Language..."
+          value={detectedLang}
         />
       </div>
       <div className="absolute bottom-0 flex justify-center items-end h-52 md:h-44">
@@ -36,4 +68,21 @@ export default function Home() {
       </div>
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  const res = await axios.get("http://localhost:3000/api/detect");
+  const { data: value } = res;
+
+  if (!value) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      value,
+    },
+  };
 }
