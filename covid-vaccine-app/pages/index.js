35d1899eb12a9157718d03e06ad839abae7b98d9
msaaddev <mrsaadirfan@gmail.com>
2021-12-04 01:36:31 +0500

    📦 NEW: Integrated API

--- covid-vaccine-app/pages/index.js
+++ covid-vaccine-app/pages/index.js
@@ -1,82 +1,164 @@
-import Head from 'next/head'
+import { useState } from "react";
+import axios from "axios";
+import Dropdown from "../components/Dropdown";
 
-export default function Home() {
+export default function Home({ data }) {
+  const [countryCode, setCountryCode] = useState("ABW");
+  const [res, setRes] = useState(data[data.length - 1]);
+
+  /**
+   *
+   *
+   * Fetch vaccine information of a country
+   */
+  const fetchInfo = async () => {
+    try {
+      const res = await axios.get(`/api/vaccine`, {
+        params: { countryCode },
+      });
+
+      const { data } = res;
+      setRes(data[data.length - 1]);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="flex flex-col items-center relative min-h-screen">
+      <h2 className="font-raleway font-bold text-6xl text-secondary pt-20 pb-6 md:text-3xl">
+        COVID-19 <span className="text-primary">Vaccine</span> Stats App
+      </h2>
+      <h3 className="text-lightGrey text-2xl font-raleway font-bold uppercase tracking-wide mb-12 md:text-base md:px-4 md:text-center">
+        Look up COVID-19 Vaccination Stats Across Different Countries
+      </h3>
+      <div className="flex justify-center items-center mb-8 w-5/6 md:items-center">
+        <Dropdown onChange={setCountryCode} />
+        <button
+          className="outline-none border border-secondary font-bold font-raleway ml-4 px-12 py-2 rounded-sm bg-secondary text-bc transition duration-300 hover:bg-bc hover:text-black md:ml-0 md:mt-4"
+          onClick={fetchInfo}
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
+          Search
+        </button>
+      </div>
+      <div className="flex flex-col text-raleway mt-6 w-3/6 h-4/5  md:flex-col md:w-4/6 md:h-full md:mb-12">
+        <table className="bg-white w-full text-secondary text-lg mb-8 md:text-sm md:mx-2">
+          <thead className="font-raleway uppercase tracking-wide">
+            <tr>
+              <th className="border border-secondary text-primary text-lg font-bold text-left px-4 py-4">
+                Information
+              </th>
+              <th className="border border-secondary text-primary text-lg font-bold text-left px-4 py-4">
+                Value
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="border border-secondary px-4 py-4">Country</td>
+              <td className="border border-secondary px-4 py-4">
+                {res.country}
+              </td>
+            </tr>
+            <tr>
+              <td className="border border-secondary px-4 py-4">Vaccines</td>
+              <td className="border border-secondary px-4 py-4">
+                {res.vaccines}
+              </td>
+            </tr>
+            <tr>
+              <td className="border border-secondary px-4 py-4">
+                Daily Vaccination Per Million
+              </td>
+              <td className="border border-secondary px-4 py-4">
+                {res.daily_vaccinations_per_million}
+              </td>
+            </tr>
+            <tr>
+              <td className="border border-secondary px-4 py-4">
+                People Fully Vaccinated
+              </td>
+              <td className="border border-secondary px-4 py-4">
+                {res.people_fully_vaccinated}
+              </td>
+            </tr>
+            <tr>
+              <td className="border border-secondary px-4 py-4">
+                People Fully Vaccinated Per Hundred
+              </td>
+              <td className="border border-secondary px-4 py-4">
+                {res.people_fully_vaccinated}
+              </td>
+            </tr>
+            <tr>
+              <td className="border border-secondary px-4 py-4">
+                People Vaccinated
+              </td>
+              <td className="border border-secondary px-4 py-4">
+                {res.people_vaccinated}
+              </td>
+            </tr>
+            <tr>
+              <td className="border border-secondary px-4 py-4">
+                People Vaccinated Per Hundred
+              </td>
+              <td className="border border-secondary px-4 py-4">
+                {res.people_vaccinated_per_hundred}
+              </td>
+            </tr>
+            <tr>
+              <td className="border border-secondary px-4 py-4">
+                Total Vaccination
+              </td>
+              <td className="border border-secondary px-4 py-4">
+                {res.total_vaccinations}
+              </td>
+            </tr>
+            <tr>
+              <td className="border border-secondary px-4 py-4">
+                Total Vaccination Per Hundred
+              </td>
+              <td className="border border-secondary px-4 py-4">
+                {res.total_vaccinations_per_hundred}
+              </td>
+            </tr>
+            <tr>
+              <td className="border border-secondary px-4 py-4">Source</td>
+              <td className="border border-secondary px-4 py-4 text-primary underline">
+                <a href={res.source_website}>{res.source_name}</a>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <div className="flex flex-col mt-4 justify-center">
+          <p className="block mb-8 text-center text-secondary text-xs">
+            Made by RapidAPI DevRel Team -{" "}
+            <a
+              className="hover:text-primary"
+              href="https://github.com/RapidAPI/DevRel-Examples-External"
+            >
+              See more examples like this
+            </a>
+          </p>
+        </div>
+      </div>
     </div>
-  )
+  );
+}
+
+export async function getServerSideProps() {
+  const res = await axios.get("http://localhost:3000/api/vaccine");
+  const { data } = res;
+
+  if (!data) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      data,
+    },
+  };
 }
