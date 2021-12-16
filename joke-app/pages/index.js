8e68834832dabface09c3dd7566881299ca0f803
msaaddev <mrsaadirfan@gmail.com>
2021-12-17 02:57:31 +0500

    📦 NEW: Integrated API

--- joke-app/pages/index.js
+++ joke-app/pages/index.js
@@ -1,82 +1,99 @@
-import Head from 'next/head'
+import axios from "axios";
+import { useState } from "react";
 
-export default function Home() {
+export default function Home({ allJokes }) {
+  const [joke, setJoke] = useState(allJokes[0]);
+  const [index, setIndex] = useState(0);
+
+  /**
+   *
+   *
+   * Show next joke
+   */
+  const nextJoke = () => {
+    if (index === 28) {
+      return;
+    }
+
+    let tempIndex = index;
+    tempIndex++;
+    setJoke(allJokes[tempIndex]);
+    setIndex(tempIndex);
+  };
+
+  /**
+   *
+   *
+   * Show previous joke
+   */
+  const prevJoke = () => {
+    if (index === 0) {
+      return;
+    }
+
+    let tempIndex = index;
+    tempIndex--;
+    setJoke(allJokes[tempIndex]);
+    setIndex(tempIndex);
+  };
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
+    <div className="flex flex-col items-center relative min-h-screen">
+      <h2 className="font-raleway font-bold text-6xl text-secondary pt-20 pb-6 md:text-3xl">
+        <span className="text-active">Jokes</span> App
+      </h2>
+      <h3 className="text-danger text-2xl font-raleway font-bold uppercase tracking-wide mb-8 md:text-base md:px-4 md:text-center">
+        An App For A Good Laugh
+      </h3>
+      <div className="flex flex-col mt-4 w-3/6 h-4/5 md:flex-col md:w-4/6 md:h-full md:mb-12">
+        <p className="mb-12 border border-secondary text-secondary font-raleway px-4 py-8 tracking-wide leading-8">
+          {joke.joke}
         </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
+        <div className="flex justify-center md:flex-col md:items-center">
+          <button
+            className="outline-none w-48 border border-danger font-bold font-raleway ml-4 px-12 py-2 rounded-sm bg-active text-primary transition duration-300 hover:bg-danger hover:text-secondary md:ml-0 md:mt-4"
+            onClick={prevJoke}
           >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
+            Previous
+          </button>
+          <button
+            className="outline-none w-48 border border-danger font-bold font-raleway ml-4 px-12 py-2 rounded-sm bg-active text-primary transition duration-300 hover:bg-danger hover:text-secondary md:ml-0 md:mt-4"
+            onClick={nextJoke}
           >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+            Next
+          </button>
         </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
+      </div>
+      <div className="flex flex-col mt-10 justify-end h-36">
+        <p className="block mb-10 text-center text-secondary text-xs">
+          Made by RapidAPI DevRel Team -{" "}
+          <a
+            className="hover:text-active"
+            href="https://github.com/RapidAPI/DevRel-Examples-External"
+          >
+            See more examples like this
+          </a>
+        </p>
+      </div>
     </div>
-  )
+  );
+}
+
+export async function getServerSideProps() {
+  const res = await axios.get("http://localhost:3000/api/joke");
+  const { data } = res;
+
+  const allJokes = data.slice(1);
+
+  if (!allJokes) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      allJokes,
+    },
+  };
 }
