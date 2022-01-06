06e335f0cdcbc1a4a516b8789a88bd182976f83b
msaaddev <mrsaadirfan@gmail.com>
2022-01-07 02:11:26 +0500

    📦 NEW: API endpoint

---
+++ random-facts-app/pages/api/fact.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+export default async function handler(req, res) {
+  if (req.method === "GET") {
+    const options = {
+      method: "GET",
+      url: "https://random-facts2.p.rapidapi.com/getfact",
+      headers: {
+        "x-rapidapi-host": "random-facts2.p.rapidapi.com",
+        "x-rapidapi-key": process.env.NEXT_PUBLIC_RAPIDAPI_KEY,
+      },
+    };
+
+    axios
+      .request(options)
+      .then(function (response) {
+        res.status(200).json(response.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  } else {
+    res.status(400);
+  }
+}
