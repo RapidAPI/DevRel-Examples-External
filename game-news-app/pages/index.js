4fe1f7b21e0d1bb5601b5d13da744a120f867089
msaaddev <mrsaadirfan@gmail.com>
2022-01-07 17:29:32 +0500

    📦 NEW: Integrated API in the application

--- game-news-app/pages/index.js
+++ game-news-app/pages/index.js
@@ -1,4 +1,7 @@
-export default function Home() {
+import axios from "axios";
+import Link from "next/link";
+
+export default function Home({ value }) {
   return (
     <div className="flex flex-col items-center relative min-h-screen">
       <h2 className="font-raleway font-bold text-6xl text-primary pt-20 pb-6 md:text-3xl">
@@ -7,6 +10,49 @@ export default function Home() {
       <h3 className="text-lightGrey text-2xl font-raleway font-bold uppercase tracking-wide mb-12 md:text-base md:px-4 md:text-center">
         Take a look at what is happening in the game world
       </h3>
+      {value.map((news) => {
+        return (
+          <div
+            className="flex items-center text-lg px-10 mb-10 font-light font-raleway h-32 w-3/6 rounded-sm border-2 border-danger text-primary cursor-pointer transition duration-300 hover:border-primary hover:text-danger md:w-80 md:h-40"
+            key={value.indexOf(news)}
+          >
+            <Link href={news.url}>
+              <a>
+                <h3>{news.title}</h3>
+                <p className="text-sm pt-4">Source: {news.source}</p>
+              </a>
+            </Link>
+          </div>
+        );
+      })}
+      <div className="flex flex-col mt-10 justify-end h-36">
+        <p className="block mb-10 text-center text-secondary text-xs">
+          Made by RapidAPI DevRel Team -{" "}
+          <a
+            className="hover:text-lightGrey"
+            href="https://github.com/RapidAPI/DevRel-Examples-External"
+          >
+            See more examples like this
+          </a>
+        </p>
+      </div>
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  const res = await axios.get("http://localhost:3000/api/news");
+  const { data: value } = res;
+
+  if (!value) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      value,
+    },
+  };
 }
