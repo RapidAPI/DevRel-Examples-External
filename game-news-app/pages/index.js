41a42d0d6039c9ecf2c455d3ff19ef28df266968
msaaddev <mrsaadirfan@gmail.com>
2022-01-07 17:22:31 +0500

    📦 NEW: First commit

---
+++ game-news-app/pages/index.js
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center relative min-h-screen">
+      <h2 className="font-raleway font-bold text-6xl text-primary pt-20 pb-6 md:text-3xl">
+        Game <span className="text-secondary">News</span> App
+      </h2>
+      <h3 className="text-lightGrey text-2xl font-raleway font-bold uppercase tracking-wide mb-12 md:text-base md:px-4 md:text-center">
+        Take a look at what is happening in the game world
+      </h3>
+    </div>
+  );
+}
