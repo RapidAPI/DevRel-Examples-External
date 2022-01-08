4ef4ab5927337360ec909a10356ef7c985459928
msaaddev <mrsaadirfan@gmail.com>
2022-01-09 01:04:27 +0500

    👌 IMPROVE: Removed async from API endpoint

--- game-news-app/pages/api/news.js
+++ game-news-app/pages/api/news.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export default async function handler(req, res) {
+export default function handler(req, res) {
   if (req.method === "GET") {
     const options = {
       method: "GET",
