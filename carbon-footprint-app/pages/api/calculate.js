4be24bc10acf947ef3c2d4732f76e8d46d0bc461
Ahmad Bilal <ahmadbilaldev@gmail.com>
2022-01-12 20:14:12 +0500

    📦 NEW: API

---
+++ carbon-footprint-app/pages/api/calculate.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+export default async function handler(req, res) {
+  const options = {
+    method: "GET",
+    url: "https://carbonfootprint1.p.rapidapi.com/CarbonFootprintFromCarTravel",
+    params: { distance: req.query.distance, vehicle: req.query.vehicle },
+    headers: {
+      "x-rapidapi-host": "carbonfootprint1.p.rapidapi.com",
+      "x-rapidapi-key": NEXT_PUBLIC_RAPIDAPI_KEY,
+    },
+  };
+
+  axios
+    .request(options)
+    .then(function (response) {
+      res.status(200).json(response.data);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+}
