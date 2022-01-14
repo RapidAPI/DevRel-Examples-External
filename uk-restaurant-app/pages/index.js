94d23849200e71974504ad6de72f4a4d1bf07f31
msaaddev <mrsaadirfan@gmail.com>
2022-01-14 22:01:33 +0500

    📦 NEW: First commit

---
+++ uk-restaurant-app/pages/index.js
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center">
+      <h2 className="font-raleway font-bold text-6xl text-primary pt-20 pb-6 md:text-3xl">
+        UK <span className="text-danger">Restaurant</span> App
+      </h2>
+      <h3 className="text-secondary text-2xl font-raleway font-bold uppercase tracking-wide mb-14 md:text-base md:px-4 md:text-center">
+        Search for Restaurants in a UK district
+      </h3>
+    </div>
+  );
+}
