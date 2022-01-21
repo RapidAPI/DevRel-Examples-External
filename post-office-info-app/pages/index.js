741636fda8dd27a5e50dc6aa81b1830a4d8e13e9
msaaddev <mrsaadirfan@gmail.com>
2022-01-21 21:47:22 +0500

    📦 NEW: First commit

---
+++ post-office-info-app/pages/index.js
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center">
+      <h2 className="font-raleway font-bold text-6xl text-primary pt-20 pb-6 md:text-3xl">
+        Post <span className="text-danger">Office</span> Finder App
+      </h2>
+      <h3 className="text-secondary text-2xl font-raleway font-bold uppercase tracking-wide mb-14 md:text-base md:px-4 md:text-center">
+        Get India Post Office Details using the pin code
+      </h3>
+    </div>
+  );
+}
