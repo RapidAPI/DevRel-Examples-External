d788efff15cb099459ebc1aaa862f3170d1f1d40
Ahmad Bilal <ahmadbilaldev@gmail.com>
2022-01-24 09:03:41 +0500

    📦 NEW: Pages

---
+++ real-estate-app/pages/_app.js
@@ -0,0 +1,20 @@
+import Head from "next/head";
+import "tailwindcss/tailwind.css";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <title>RapidAPI Devrel Example - Real Estate App</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
