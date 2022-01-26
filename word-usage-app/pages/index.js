d8039fe9e3390c065d50b0b239fd21c8026dca4f
msaaddev <mrsaadirfan@gmail.com>
2022-01-26 22:31:47 +0500

    📦 NEW: First commit

---
+++ word-usage-app/pages/index.js
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center relative min-h-screen">
+      <h2 className="font-raleway font-bold text-6xl text-primary pt-20 pb-6 md:text-3xl">
+        Word <span className="text-secondary">Usage</span> App
+      </h2>
+      <h3 className="text-secondary text-2xl font-raleway font-bold uppercase tracking-wide mb-12 md:text-base md:px-4 md:text-center">
+        Shows How A Word Is Used In a Sentence
+      </h3>
+    </div>
+  );
+}
