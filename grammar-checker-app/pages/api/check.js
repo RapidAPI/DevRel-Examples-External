89f8315f37ff852f881badc09e99ffc70bc694dc
msaaddev <mrsaadirfan@gmail.com>
2022-02-03 15:44:18 +0500

    📦 NEW: Created API endpoint

---
+++ grammar-checker-app/pages/api/check.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+export default function handler(req, res) {
+  if (req.method === "POST") {
+    const options = {
+      method: "POST",
+      url: "https://grammarbot.p.rapidapi.com/check",
+      headers: {
+        "content-type": "application/x-www-form-urlencoded",
+        "x-rapidapi-host": "grammarbot.p.rapidapi.com",
+        "x-rapidapi-key": process.env.NEXT_PUBLIC_RAPIDAPI_KEY,
+      },
+      data: { text: req.body.text, language: "en-US" },
+    };
+
+    axios
+      .request(options)
+      .then(function (response) {
+        res.status(200).json(response.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  } else {
+    res.status(400);
+  }
+}
