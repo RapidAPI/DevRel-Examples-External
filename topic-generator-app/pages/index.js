2360ccdee33dbd4e5f2628bc74e43a5a73aeeda0
msaaddev <mrsaadirfan@gmail.com>
2022-02-10 14:30:09 +0500

    📦 NEW: First commit

---
+++ topic-generator-app/pages/index.js
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center relative min-h-screen">
+      <h2 className="font-raleway font-bold text-6xl text-primary pt-20 pb-6 md:text-3xl">
+        Topic <span className="text-secondary">Generator</span> App
+      </h2>
+      <h3 className="text-danger text-2xl font-raleway font-bold uppercase tracking-wide mb-12 md:text-base md:px-4 md:text-center">
+        Automatically generate topics and keywords for articles and blogs
+      </h3>
+    </div>
+  );
+}
