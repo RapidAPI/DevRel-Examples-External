eb5d56d554d91ca3ab0b89588b4f1d3c647f8d96
Ahmad Bilal <ahmadbilaldev@gmail.com>
2022-02-12 13:48:44 +0500

    📦 NEW: Configs

---
+++ lyrics-app/tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      poppins: ["Poppins", "sans-serif"],
+    },
+    colors: {
+      background: "#000000",
+      primary: "#FFFFFF",
+      secondary: "#9E9E9E",
+      active: "#F40812",
+      light: "#121212",
+    },
+  },
+
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
