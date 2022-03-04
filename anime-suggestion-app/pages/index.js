36d702f5caac8cf10ce22ce4e522ffe5a72ba12d
msaaddev <mrsaadirfan@gmail.com>
2022-03-04 14:21:40 +0500

    📦 NEW: First commit

---
+++ anime-suggestion-app/pages/index.js
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center relative min-h-screen">
+      <h2 className="font-raleway font-bold text-6xl text-primary pt-20 pb-6 md:text-3xl">
+        Anime <span className="text-secondary">Suggestion</span> App
+      </h2>
+      <h3 className="text-lightGrey text-2xl font-raleway font-bold uppercase tracking-wide mb-12 md:text-base md:px-4 md:text-center">
+        Get anime suggestion based on your favorite anime
+      </h3>
+    </div>
+  );
+}
