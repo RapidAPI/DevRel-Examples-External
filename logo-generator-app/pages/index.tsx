ff4c8c4e0e570ce2b253ac8337f278437f7578a3
msaaddev <mrsaadirfan@gmail.com>
2023-01-23 22:01:45 +0500

    📦 NEW: Created initial UI

--- logo-generator-app/pages/index.tsx
+++ logo-generator-app/pages/index.tsx
@@ -1,86 +1,97 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { NextPage } from "next";
+import Head from "next/head";
+import React, { useState } from "react";
 
 const Home: NextPage = () => {
+  const [hexValueInputBox, setHexValueInputBox] = useState<number[]>([1]);
+  const [hexValues, setHexValues] = useState<string[]>([]);
+
+  /**
+   * Get brand color hex values from user
+   *
+   *
+   * @param {React.ChangeEvent<HTMLInputElement>} e
+   */
+  const handleHexValues = (e: React.ChangeEvent) => {
+    const { value } = e.target as HTMLInputElement;
+    value.length === 6 && setHexValues([...hexValues, value]);
+  };
+
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>LogoAI</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold font-sans">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
+        <h1 className="text-7xl font-bold font-sans text-[#0055d9]">LogoAI</h1>
+        <p className="mt-8 text-2xl w-2/5 text-[#081477] font-bold">
+          Create unique and professional logos for your company using AI
         </p>
 
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
+        <div className="mt-12 flex max-w-xl flex-col flex-wrap sm:w-full border border-gray-300 p-6 rounded-md shadow-sm">
+          <div>
+            <div className="flex">
+              <p className="flex items-center justify-center rounded-lg w-[30px] h-[30px] bg-[#ef386a] text-white">
+                <span className="text-sm">1</span>
+              </p>
+              <p className="mt-1 ml-2">
+                Please provide your brand colors hex values
+              </p>
+            </div>
+            <div className="w-full text-left ml-[38px]">
+              {hexValueInputBox.map((item) => (
+                <input
+                  key={item}
+                  type="number"
+                  style={{
+                    appearance: "none",
+                    WebkitAppearance: "none",
+                    MozAppearance: "textfield",
+                  }}
+                  placeholder="Hex value"
+                  className="mr-2 my-2 text-sm rounded-md w-[100px] border border-gray-300 shadow-sm p-2 focus:ring-black"
+                  onChange={handleHexValues}
+                />
+              ))}
 
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+              <button
+                className="w-[30px] h-[30px] bg-[#081477] rounded-[999px] text-white"
+                onClick={() => {
+                  setHexValueInputBox([...hexValueInputBox, 1]);
+                }}
+              >
+                +
+              </button>
+            </div>
+          </div>
+          <div className="mt-6">
+            <div className="flex">
+              <p className="flex items-center justify-center rounded-lg w-[30px] h-[30px] bg-[#ef386a] text-white">
+                <span className="text-sm">2</span>
+              </p>
+              <p className="mt-1 ml-2">
+                Please provide a short description of your company
+              </p>
+            </div>
+            <div className="w-full text-left ml-[38px]">
+              <textarea
+                className="w-5/6 h-[200px] mt-2 text-sm rounded-md border border-gray-300 shadow-sm p-2 focus:ring-black"
+                placeholder="1000 characters max"
+                maxLength={1000}
+              />
+            </div>
+            <div className="mt-6 flex justify-center w-full">
+              <button className="w-full py-2 md:text-sm bg-[#081477] rounded-md text-white">
+                Generate logo
+              </button>
+            </div>
+          </div>
         </div>
       </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
