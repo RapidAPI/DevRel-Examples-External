878e33ab9409237123cc4bf5c766f7c4d2b634e7
msaaddev <mrsaadirfan@gmail.com>
2023-01-26 01:04:56 +0500

    📦 NEW: Added images domain

--- logo-generator-app/next.config.js
+++ logo-generator-app/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  images: {
+    domains: ["oaidalleapiprodscus.blob.core.windows.net"],
+  },
   experimental: {
     fontLoaders: [
       { loader: "@next/font/google", options: { subsets: ["latin"] } },
